c05c5998a27effabc876322582165262f9bbe85e
nbeney <nbeney@gmail.com>
2023-05-18 07:33:40 +0100

    Add SearchUtils::isSorted.

--- src/main/java/org/example/SearchUtils.java
+++ src/main/java/org/example/SearchUtils.java
@@ -47,4 +47,13 @@ public class SearchUtils {
                     : dichotomicSearchRecursiveRun(elements, value, mid + 1, end);
         }
     }
+
+    public static boolean isSorted(final int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/org/example/SearchUtilsTest.java
+++ src/test/java/org/example/SearchUtilsTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
@@ -9,6 +10,22 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchUtilsTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            ":true",
+            "1:true",
+            "1,1:true",
+            "1,2:true",
+            "1,2,3:true",
+            "1,0:false",
+            "1,2,3,0:false",
+    }, delimiter = ':')
+    void checkIsSorted(final String arrayText, final String expectedText) {
+        final int[] array = parseArrayText(arrayText, ",");
+        final boolean expected = Boolean.parseBoolean(expectedText);
+        assertEquals(expected, SearchUtils.isSorted(array));
+    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/SearchUtilsTest/data.csv", delimiter = ':')
@@ -29,12 +46,16 @@ class SearchUtilsTest {
     }
 
     private static void check(final String arrayText, final String valueText, final String expectedText, final BiFunction<int[], Integer, Integer> search) {
-        final int[] array = arrayText == null
-                ? new int[]{}
-                : Stream.of(arrayText.split(",")).mapToInt(Integer::parseInt).toArray();
+        final int[] array = parseArrayText(arrayText, ",");
         final int value = Integer.parseInt(valueText);
         final int expected = Integer.parseInt(expectedText);
         final int actual = search.apply(array, value);
         assertEquals(expected, actual);
+    }
+
+    private static int[] parseArrayText(String arrayText, final String delimiter) {
+        return arrayText == null
+                ? new int[]{}
+                : Stream.of(arrayText.split(delimiter)).mapToInt(Integer::parseInt).toArray();
     }
 }
