858922e4d20b56e3a498720547d3112421b18f1c
nbeney <nbeney@gmail.com>
2023-05-19 17:31:31 +0100

    Add demo with Strings.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,41 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        final Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-        final int value = 5;
-
-        System.out.println("linearSearch --> " + SearchUtils.linearSearch(arr, value));
-        System.out.println("binarySearchIterative --> " + SearchUtils.binarySearchIterative(arr, value));
-        System.out.println("binarySearchRecursive --> " + SearchUtils.binarySearchRecursive(arr, value));
-        System.out.println("isSorted --> " + SearchUtils.isSorted(arr));
+        demoWithIntegers();
+        System.out.println();
+        demoWithStrings();
 
         // Check SearchUtilsTest for more examples.
+    }
+
+    private static void demoWithIntegers() {
+        final Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+        final int value = 5;
+
+        System.out.println("DEMO WITH INTEGERS");
+        System.out.println("==================");
+        System.out.println("array = " + Arrays.toString(array));
+        System.out.println("value = " + value);
+        System.out.println("isSorted --> " + SearchUtils.isSorted(array));
+        System.out.println("linearSearch --> " + SearchUtils.linearSearch(array, value));
+        System.out.println("binarySearchIterative --> " + SearchUtils.binarySearchIterative(array, value));
+        System.out.println("binarySearchRecursive --> " + SearchUtils.binarySearchRecursive(array, value));
+    }
+
+    private static void demoWithStrings() {
+        final String[] array = {"Apple", "Bread", "Cake", "Donuts", "Eggs", "Fish"};
+        final String value = "Donuts";
+
+        System.out.println("DEMO WITH STRINGS");
+        System.out.println("=================");
+        System.out.println("array = " + Arrays.toString(array));
+        System.out.println("value = " + value);
+        System.out.println("isSorted --> " + SearchUtils.isSorted(array));
+        System.out.println("linearSearch --> " + SearchUtils.linearSearch(array, value));
+        System.out.println("binarySearchIterative --> " + SearchUtils.binarySearchIterative(array, value));
+        System.out.println("binarySearchRecursive --> " + SearchUtils.binarySearchRecursive(array, value));
     }
 }
